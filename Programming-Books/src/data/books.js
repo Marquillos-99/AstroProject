33187455933fb471a45982047864e8aa895e91a0
Marquillos-99 <marckgit@gmail.com>
2023-07-13 13:49:54 -0600

    404

---
+++ Programming-Books/src/data/books.js
@@ -0,0 +1,32 @@
+export const books = [
+    {
+        id:'the-clean-coder',
+        title:'The Clean Coder: A Code of Conduct for Professional Programmers',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg"
+    },
+    {
+        id:'the-pragmatic-programer',
+        title:'The Pragmatic Programmer: From Journeyman to Master',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg"
+    },
+    {
+        id:'code-complete',
+        title:'Code Complete',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg"
+    },
+    {
+        id:'introduction-to-algoritmhs',
+        title:'Introduction to Algorithms',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1387741681i/108986.jpg"
+    },
+    {
+        id:'efective-java',
+        title:'Effective Java',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1513389229i/34927404.jpg"
+    },
+    {
+        id:'eloquent',
+        title:'Eloquent JavaScript: A Modern Introduction to Programming',
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+    }
+]
