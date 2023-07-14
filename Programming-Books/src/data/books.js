c9a4324d6e1507f4bcd4a9ada006a9369e9ae28e
Marquillos-99 <marckgit@gmail.com>
2023-07-14 13:59:01 -0600

    opinions

--- Programming-Books/src/data/books.js
+++ Programming-Books/src/data/books.js
@@ -3,42 +3,42 @@ export const books = [
         id:'the-clean-coder',
         title:'The Clean Coder: A Code of Conduct for Professional Programmers',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
-        opinion:'',
+        opinion:'The first half of this book is well worth a read. Then I was reminded of Martin Fowlers (I think) comment that the original Design Patterns Elements of Reusable Software book was a response to the limitations of C++. It dovetailed so well into Java because Java has a lot of the same annoying limitations, and in some ways is even harder. by Francis Fish',
         author:'Robert C. Martin'
     },
     {
         id:'the-pragmatic-programer',
         title:'The Pragmatic Programmer: From Journeyman to Master',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg",
-        opinion:'',
+        opinion:'"While many complain about already knowing everything in the book, or that its outdated, I believe they are quite missing the point. Perhaps this book didnt speak to you at the point you are at in developing your skills and crafts, but it might speak to someone else just beginning. Rating the book low for the reason it wasnt what you needed is rather disingenuous, as a rating should be a guide to the quality of the book overall. by Tamara"',
         author:'Andy Hunt, Dave Thomas'
     },
     {
         id:'code-complete',
         title:'Code Complete',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg",
-        opinion:'',
+        opinion:'"Code Complete is a massive work, so this summary is, necessarily, very high level. It is not a book that one can absorb completely in one reading, but one can absorb its high level themes (summarized nicely in the second to last chapter)." by Erika RS',
         author:'Steve McConnell'
     },
     {
         id:'introduction-to-algoritmhs',
         title:'Introduction to Algorithms',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1387741681i/108986.jpg",
-        opinion:'',
+        opinion:'"An essential book for every programmer, you cant read this kind of book on bus, you need to fully constraint while reading it. The exercises after each chapter are very important to fully understand the chapter you just read, and to activate your brains neurons. The book in itself is an outstanding one, very organized, focused and small chapters makes it easier to understand the algorithms inside it. by Khaled Alhoran',
         author:'Thomas H. Cormen, Charles E. Leiserson, Ronald R. Rivest, Clifford Stein'
     },
     {
         id:'efective-java',
         title:'Effective Java',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1513389229i/34927404.jpg",
-        opinion:'',
+        opinion:'"Effective Java: even the title is concise and to-the-point, like the rest of the books writing. This is not a book for beginners, but rather a book on general software design concepts, particularly with respect to Java itself. by Andrew"',
         author:'Josua Bloch'
     },
     {
         id:'eloquent',
         title:'Eloquent JavaScript: A Modern Introduction to Programming',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
-        opinion:'',
+        opinion:'"Im sure those fancy JS wranglers out there will get a lot out of this book, but if youre a coding newbie, look elsewhere for your intro, far far elsewhere. I made the mistake of thinking Id use this book to help supplement my learning of JS. And what a mistake. It gave no practical help whatsoever. by Nico"',
         author:'Marikjin Haverbeke'
     }
 ]
