9a52343966e4ad128774fbc03c8a207ca53db801
Marquillos-99 <marckgit@gmail.com>
2023-07-14 13:38:06 -0600

    authors

--- Programming-Books/src/data/books.js
+++ Programming-Books/src/data/books.js
@@ -2,31 +2,43 @@ export const books = [
     {
         id:'the-clean-coder',
         title:'The Clean Coder: A Code of Conduct for Professional Programmers',
-        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg"
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+        opinion:'',
+        author:'Robert C. Martin'
     },
     {
         id:'the-pragmatic-programer',
         title:'The Pragmatic Programmer: From Journeyman to Master',
-        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg"
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg",
+        opinion:'',
+        author:'Andy Hunt, Dave Thomas'
     },
     {
         id:'code-complete',
         title:'Code Complete',
-        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg"
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1396837641i/4845.jpg",
+        opinion:'',
+        author:'Steve McConnell'
     },
     {
         id:'introduction-to-algoritmhs',
         title:'Introduction to Algorithms',
-        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1387741681i/108986.jpg"
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1387741681i/108986.jpg",
+        opinion:'',
+        author:'Thomas H. Cormen, Charles E. Leiserson, Ronald R. Rivest, Clifford Stein'
     },
     {
         id:'efective-java',
         title:'Effective Java',
-        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1513389229i/34927404.jpg"
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1513389229i/34927404.jpg",
+        opinion:'',
+        author:'Josua Bloch'
     },
     {
         id:'eloquent',
         title:'Eloquent JavaScript: A Modern Introduction to Programming',
         img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+        opinion:'',
+        author:'Marikjin Haverbeke'
     }
 ]
